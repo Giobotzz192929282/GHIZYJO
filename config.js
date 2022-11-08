876d9f25b20cfcc23ad70cd0cf1cc96ae8113bd4
Giobotzz192929282 <114280104+Giobotzz192929282@users.noreply.github.com>
2022-11-08 10:00:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['6282269718172','6282269718172','6282269718172']
+global.owner = ['6282269718172','6283825704855','6282269718172']
 global.premium = ['6282269718172']
 global.packname = 'The GIO Bot'
 global.author = 'WhatsApp Bot'
